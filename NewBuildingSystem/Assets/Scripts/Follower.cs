3eb6054a97137f92c324b67b3049898a7712180d
lakman <lakman@stud.hs-heilbronn.de>
2018-10-22 21:50:05 +0200

    initial commit

---
+++ NewBuildingSystem/Assets/Scripts/Follower.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Follower : MonoBehaviour {
+
+    public Transform endMarker;
+
+    public float speed;
+	
+	void FixedUpdate () {
+        transform.position = Vector3.Lerp(transform.position, endMarker.position, speed * Time.fixedDeltaTime);
+    }
+}
